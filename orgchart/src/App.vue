8a352974c2847d548197c43e9e460c0ce2cad38c
ducle91 <ducle1991@gmail.com>
2018-08-24 01:24:45 -0400

    initial commit

---
+++ orgchart/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/tnttlogo.png">
+    <h1>Welcome to ADLNC TNTT!</h1>
+    <orgchart/>
+  </div>
+</template>
+
+<script>
+import orgchart from './components/orgchart.vue'
+
+export default {
+  name: 'app',
+  components: {
+    orgchart
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ orgchart/src/components/orgchart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <vo-basic :data="chartData" :draggable="true" :pan="true" :zoom="true"></vo-basic>
+  </div>
+</template>
+
+<script>
+import { VoBasic } from 'vue-orgchart'
+export default {
+  components: { VoBasic },
+  created () {
+    this.chartData = {
+      name: 'Doan Truong - Thinh Tran',
+        children: [
+          { name: 'Angular' },
+          {
+            name: 'React',
+            children: [{ name: 'Preact' }]
+          },
+          {
+            name: 'Vue',
+            children: [{ name: 'Moon' }]
+          }
+        ]
+    }
+}
+}
+</script>
+
